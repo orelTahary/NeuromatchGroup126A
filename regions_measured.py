dea27bd42fffbe6c45004f0d5ae81dab5a4e55a8
Taha Solakoglu <47305046+sabotaha@users.noreply.github.com>
2020-07-23 03:19:43 +0300

    Create regions_measured.py

---
+++ regions_measured.py
@@ -0,0 +1,20 @@
+def regions_measured(session_num):
+  '''
+  Checks which brain areas were recorded from given session.
+
+  Arg:
+  session_num: number of the session (0,38)
+
+  Output: 
+  sessions_regions: brains areas that recording had been taken
+
+  '''
+  regions=[]
+  for i in range(len(alldat)):
+    regions.append(np.unique(alldat[i]['brain_area']))
+
+  sessions_regions=regions[session_num]
+
+  print(sessions_regions)
+
+  return regions,sessions_regions
