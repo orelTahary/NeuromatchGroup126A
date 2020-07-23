0a86553aa26580c7d018c8c9f25cef47bd913706
GiuliaBallardini <57895912+GiuliaBallardini@users.noreply.github.com>
2020-07-23 16:32:03 +0200

    Update compute_mov_onset.py

--- compute_mov_onset.py
+++ compute_mov_onset.py
@@ -12,15 +12,24 @@ sample_stim=50 # sample where the stimulus occured (-> after 500ms, bin time=10 
 # # Threshold for considering the 'wheel as turned' 
 threshold=5
 # the trials that you want to consider
-trials_interval=np.array([5, 6, 7, 8, 9])#np.array(range(0,340))  
+trials_interval=np.array(range(0,340))  
 
 # Initialization
 n_trials=len(trials_interval)
-mov_onset=np.zeros(n_trials)  #sample where the wheel is above the threshold for the first time; No turn-> mov_onset = 0
-direction=np.zeros(n_trials) #<0 chosoe right (move left?); =0 No turn; >0 choose left (turn right?)
-onset_gocue=np.zeros(n_trials) #<0 before gocue; =0 No turn, >0 after gocue
+sample_stim=50
 
-j=0
+## --- Settable parameter:  
+# Threshold of when considered the 'wheel turned' 
+threshold=5 
+# Trials that you want to check
+trials_interval=np.array(range(0,340))  
+
+# Initialization
+n_trials=len(trials_interval)
+mov_onset=[] #time in ms of when the mice moves for the first time the wheel given the threshold above
+trial_NoWheel=[] #indeces of the trials where the mice did not turn the wheel 
+direction=[] #<0 choose right (turn left?); =0 No turn; >0 choose left (turn right?);
+onset_gocue=[] #<0 before gocue; >0 after gocue
 
 for i in trials_interval:
   sample_go=int(stimulus+gocue[i]*100)
@@ -29,18 +38,18 @@ for i in trials_interval:
   idx=np.argwhere(abs(wheel_trial)>threshold) # index when the wheel is more turned than the threshold
 
   if len(idx)==0:
-    mov_onset[j]=0 
-    direction[j]=0
-    onset_gocue[j]=0
+    trial_NoWheel.append(i)
+    direction.append(0)
   else:
-    mov_onset[j]=idx[0]+sample_stim
-    direction[j]=np.sign(wheel_trial[idx[0]]) 
-    onset_gocue[j]=np.sign(mov_onset[j]-sample_go)
-  j=j+1
-  # print(mov_onset)
-  # ax=plt.subplot(2,5,i-9)
-  # plt.plot(wheel[0,i,:],color='b')
-  # plt.axvline(mov_onset[i],color='k')
+    mov_onset.append(idx[0]*10) #in ms
+    onset_gocue.append(np.sign(mov_onset[j]-sample_go))
+    direction.append(np.sign(wheel_trial[idx[0]]))
+   
+plt.hist(mov_onset)
+
+# print(len(resp[resp==0]))
+# print(len(mov_onset))
+# print(trial_NoWheel)
  
 # plt.plot(mov_onset)
 # print(mov_onset)
